6e3f03ea2b6bf76d631671eab774e66e6c5bf8f4
Manan Kansara <manan.k@netizens.email>
2024-06-14 17:38:16 +0530

    changes 1.1

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -48,33 +48,7 @@ const Navbar: React.FC = () => {
                       done.
                     </SheetDescription>
                   </SheetHeader>
-                  <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                      <Label htmlFor="name" className="text-right">
-                        Name
-                      </Label>
-                      <Input
-                        id="name"
-                        placeholder="Your name"
-                        className="col-span-3"
-                      />
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                      <Label htmlFor="username" className="text-right">
-                        Username
-                      </Label>
-                      <Input
-                        id="username"
-                        value="@peduarte"
-                        className="col-span-3"
-                      />
-                    </div>
-                  </div>
-                  <SheetFooter>
-                    <SheetClose asChild>
-                      <Button type="submit">Save changes</Button>
-                    </SheetClose>
-                  </SheetFooter>
+                    
                 </SheetContent>
               </Sheet>
             </div>
